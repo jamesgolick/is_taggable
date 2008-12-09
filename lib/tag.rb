faaaf1f7cd08431949ca4befbf11655c40294526
James Golick <james@giraffesoft.ca>
2008-12-08 19:18:01 -0500

    add uniqueness constraint to tag

--- lib/tag.rb
+++ lib/tag.rb
@@ -1,3 +1,5 @@
 class Tag < ActiveRecord::Base
   has_many :taggings
+
+  validates_uniqueness_of :name
 end
--- test/tag_test.rb
+++ test/tag_test.rb
@@ -4,4 +4,8 @@ Expectations do
   expect Tagging do
     Tag.new.taggings.proxy_reflection.klass
   end
+
+  expect Tag.new(:name => "duplicate").not.to.be.valid? do
+    Tag.create!(:name => "duplicate")
+  end
 end
