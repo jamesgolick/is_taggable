c680381b189980ff701f8efc0541a3c0eb5df891
James Golick <james@giraffesoft.ca>
2008-12-08 19:07:04 -0500

    add some models

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,6 +12,17 @@ ActiveRecord::Schema.define(:version => 0) do
   create_table :posts do |t|
     t.string  :title, :default => ''
   end
+
+  create_table :tags do |t|
+    t.string :name, :default => ''
+  end
+
+  create_table :taggings do |t|
+    t.integer :tag_id
+
+    t.string  :taggable_type, :default => ''
+    t.integer :taggable_id
+  end
 end
 
 class Post < ActiveRecord::Base
